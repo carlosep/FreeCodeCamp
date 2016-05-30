ef95b4549b264f283cff5ccdefe735197c57c726
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-05-30 14:16:01 -0300

    Build a wikipedia viewer challenge done

--- Front End Development Certification/Intermediate Front End Development Projects/Build a Wikipedia Viewer/pen.js
+++ Front End Development Certification/Intermediate Front End Development Projects/Build a Wikipedia Viewer/pen.js
@@ -1,23 +1,22 @@
-
 $(function() {
   var firstClick = true;
+  var $results = $('#results');
 $("#searchButton").click(function(){
+  var searchBar = $("#searchBar");
   if(firstClick){
     var button = $("#searchButton");
     button.animate({width: '50px'}, "fast");
     button.css({'border-radius': "0px 5px 5px 0px"});
     button.html('GO');
-    $("#searchBar").animate({opacity: 1}, "slow");
-    $("#searchBar").show();
+    searchBar.animate({opacity: 1}, "slow");
+    searchBar.show();
     firstClick = false;
   }else{
-    console.log(getResults('test'))
+    $results.empty();
+    getResults(searchBar.val());
   }
 });
 
-
-
-  var $results = $('#results');
   function getResults(exp) {
     $.ajax({
       url: 'https://www.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?',
@@ -29,9 +28,23 @@ $("#searchButton").click(function(){
         search: exp,
       },
       success: function(data) {
-        $.each(data, function(key, value){
-          $results.append("AQUELE LAYOUT QUE VC TA LIGADO")
-        });
+        for(var i = 0; i < 10; i++){
+          var title, body, link;
+          for(var j = 1; j < 4; j++){
+            switch(j){
+              case 1:
+                title = data[j][i];
+                break;
+              case 2:
+                body = data[j][i];
+                break;
+              case 3:
+                link = data[j][i];
+                break;
+            }
+          }
+          $results.append('<a href='+link+' target="_blank"><div class="col-lg-12"><h1>' + title + '</h1><p>' + body + '</p></div></a>');
+        }
       }
     });
   }
