b181057ff0a87193f7c5fc0b162311181ccf736d
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-06-01 16:30:43 -0300

    Convert HTML challenge done

---
+++ Front End Development Certification/Intermediate Algorithm Scripting/convertHtmlEntities.js
@@ -0,0 +1,19 @@
+function convertHTML(str) {
+  // &colon;&rpar;
+  return str.replace(/&|<|>|"|'/g, function(match){
+    switch(match){
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '>':
+        return '&gt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&apos;';
+    }
+  });
+}
+
+convertHTML("Dolce & Gabbana");
