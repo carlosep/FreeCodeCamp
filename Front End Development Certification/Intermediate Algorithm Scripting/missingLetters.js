6bdeb90f485129db91bc39f853cd782f4c007494
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-06-01 14:20:32 -0300

    Missing letters adjustments

--- Front End Development Certification/Intermediate Algorithm Scripting/missingLetters.js
+++ Front End Development Certification/Intermediate Algorithm Scripting/missingLetters.js
@@ -1,7 +1,6 @@
 
 function fearNotLetter(str) {
   var first = str.charCodeAt(0);
-  var last = str.charCodeAt(str.length - 1);
 
   for(var j = 0; j < str.length; j++){
     if(str[j] == String.fromCharCode(first)){
