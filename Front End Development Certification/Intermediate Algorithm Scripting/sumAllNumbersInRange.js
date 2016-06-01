56f5f11f48fb064301782cb63218f8371c4664b1
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-06-01 14:39:34 -0300

    Sum All Numbers & Boo who challenge done

---
+++ Front End Development Certification/Intermediate Algorithm Scripting/sumAllNumbersInRange.js
@@ -0,0 +1,13 @@
+
+function sumAll(arr) {
+  arr.sort(function(a,b){
+    return a - b;
+  });
+
+  var sum = 0;
+  for(var i = arr[0]; i <= arr[1]; i++) {
+   sum += i;
+  }
+  return sum;
+}
+sumAll([1, 4]);
